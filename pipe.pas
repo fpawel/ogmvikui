2ee7bc9959b8691ed78eb9429d928ca0e98d0cec
fpawel <binf@mail.ru>
2018-10-22 10:15:50 +0300

    fix pipe str

--- pipe.pas
+++ pipe.pas
@@ -2,34 +2,40 @@ unit pipe;
 
 interface
 
-
+uses Winapi.Windows;
 
 type
     TPipeClient = class
     private
         { Private declarations }
         hPipe: THANDLE; // дескриптор
-        buf : array [0..1000] of byte;
+        buf: array [0 .. 1000] of byte;
+
+        procedure thisWriteFile(var Buffer;
+          numberOfbytesToWrite: DWORD);
+        procedure thisReadFile(var Buffer; numberOfbytesToRead: DWORD);
     public
         { Public declarations }
-        Constructor Create(pipe_server : string);
-        procedure WriteInt(v:LONGWORD);
-        function ReadInt:LONGWORD;
+        Constructor Create(pipe_server: string);
+        procedure WriteInt(v: LONGWORD);
+        function ReadInt: LONGWORD;
 
-        procedure WriteString(s:string);
-        function ReadString:string;
+        procedure WriteString(s: string);
+        function ReadString: string;
     end;
 
 implementation
 
-uses sysutils, Winapi.Windows, System.WideStrUtils;
+uses sysutils,  System.WideStrUtils;
 
+type
+    _LONGWORD_BYTES = record
 
-type _LONGWORD_BYTES = record
-
-    case Integer of
-      0: (bytes: array [0..3] of BYTE );
-      1: (value: longword);
+        case Integer of
+            0:
+                (bytes: array [0 .. 3] of byte);
+            1:
+                (value: LONGWORD);
     end;
 
 procedure terminate_error(error_text: string);
@@ -43,87 +49,110 @@ begin
     ExitProcess(1);
 end;
 
-Constructor TPipeClient.Create(pipe_server : string);
+Constructor TPipeClient.Create(pipe_server: string);
 begin
-    hPipe := CreateFileW(PWideChar('\\.\pipe\$'+ pipe_server + '$'), GENERIC_READ or
-      GENERIC_WRITE, FILE_SHARE_READ or FILE_SHARE_WRITE, nil,
+    hPipe := CreateFileW(PWideChar('\\.\pipe\$' + pipe_server + '$'),
+      GENERIC_READ or GENERIC_WRITE, FILE_SHARE_READ or FILE_SHARE_WRITE, nil,
       OPEN_EXISTING, 0, 0);
     if hPipe = INVALID_HANDLE_VALUE then
         terminate_error('hPipe = INVALID_HANDLE_VALUE');
 
 end;
 
-function TPipeClient.ReadInt:LONGWORD;
-var x : _LONGWORD_BYTES;
+procedure TPipeClient.thisReadFile(var Buffer; numberOfbytesToRead: DWORD);
+var
     readed_count: DWORD;
 begin
-    if not ReadFile(hPipe, x.bytes, 4, readed_count, nil) then begin
+    if not ReadFile(hPipe, Buffer, numberOfbytesToRead, readed_count, nil) then
+    begin
+        terminate_error('ReadFile');
+    end;
+
+    if readed_count <> numberOfbytesToRead then
+    begin
+        terminate_error(Format('ReadFile: readed_count=%d, must be %d',
+          [readed_count, numberOfbytesToRead]));
+    end;
+end;
+
+function TPipeClient.ReadInt: LONGWORD;
+var
+    x: _LONGWORD_BYTES;
+    readed_count: DWORD;
+begin
+    if not ReadFile(hPipe, x.bytes, 4, readed_count, nil) then
+    begin
         terminate_error('ReadInt: ReadFile error');
     end;
-    if readed_count <> 4 then begin
+    if readed_count <> 4 then
+    begin
         terminate_error('ReadInt: readed_count <> 4');
     end;
     result := x.value;
 end;
 
-procedure TPipeClient.WriteInt(v:LongWord);
+procedure TPipeClient.WriteInt(v: LONGWORD);
 var
     writen_count: DWORD;
-    x : _LONGWORD_BYTES;
+    x: _LONGWORD_BYTES;
 
 begin
     x.value := v;
-    if not(WriteFile(hPipe, x.bytes, 4, writen_count, nil)) then begin
+    if not(WriteFile(hPipe, x.bytes, 4, writen_count, nil)) then
+    begin
         terminate_error('WriteInt: WriteFile error');
     end;
-    if writen_count <> 4 then begin
+    if writen_count <> 4 then
+    begin
         terminate_error('WriteInt: writen_count <> 4');
     end;
 end;
 
-
-procedure TPipeClient.WriteString(s:string);
-var writen_count: DWORD;
-    i,len : Cardinal;
-
+procedure TPipeClient.thisWriteFile(var Buffer; numberOfbytesToWrite: DWORD);
+var
+    writen_count: DWORD;
 begin
-    len := Length(s) * sizeof(char);
-    if len >= Length(self.buf) + 1 then begin
-        raise Exception.Create('Превышен размер буфера');
+    
+    if not(WriteFile(hPipe, Buffer, numberOfbytesToWrite, writen_count, nil))
+    then
+    begin
+        terminate_error('WriteFile');
     end;
-    self.WriteInt(len);
-    if len=0 then
-        exit;
-
-    Move(s[1], self.buf, len);
-
-    if not(WriteFile(hPipe,  self.buf, len, writen_count, nil)) then begin
-        terminate_error('WriteString: WriteFile error');
-    end;
-    if writen_count <> len then begin
-        terminate_error('WriteString: writen_count <> 1');
+    if writen_count <> numberOfbytesToWrite then
+    begin
+        terminate_error(Format('WriteFile: writen_count=%d, must be %d',
+          [writen_count, numberOfbytesToWrite]));
     end;
 end;
 
-function TPipeClient.ReadString:string;
-var readed_count: DWORD; len : LongWord;
-    b:array [0..1000] of byte;
+
+
+procedure TPipeClient.WriteString(s: string);
+var
+    len: Integer;
+    ptr_bytes: TBytes;
+
+begin
+    ptr_bytes := WideBytesOf(s);
+    len := Length(ptr_bytes);
+    WriteInt(len);
+    thisWriteFile(ptr_bytes[0], len);
+end;
+
+function TPipeClient.ReadString: string;
+var
+    readed_count: DWORD;
+    len: LONGWORD;
+    i: Integer;
+    Buffer: TBytes;
+    Str: string;
 begin
     len := ReadInt;
-    if not ReadFile(hPipe, b, len, readed_count, nil) then begin
-        terminate_error('ReadString: ReadFile error');
-    end;
-    if readed_count <> len then begin
-        terminate_error(Format('ReadString: readed_count = %d <> str len = %d', [readed_count, len]));
-    end;
-    SetString(Result, PAnsiChar(@b[0]), len);
-    Finalize(b);
-
+    if len = 0 then
+        exit('');
+    SetLength(Buffer, len);
+    thisReadFile(Buffer[0], len);
+    result := WideStringOf(Buffer);
 end;
-
-
-
-
-
 
 end.
